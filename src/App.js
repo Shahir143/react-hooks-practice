1daedbef00e3e500739c89dd321fac0aad39a2bb
shahir143 <Shahirsyed143@gmail.com>
2024-06-13 22:52:41 +0530

    useEffect function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  //! const [counter, setCounter] = useState(0);
-  // - const [name, setName] = useState();
-  const [details, setDetails] = useState({ counter: 0, name: "" });
+  const [time, setTime] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime((prevTime) => {
+        const newTime = prevTime === 10 ? 0 : prevTime + 1;
+        console.log(newTime);
+        document.title = `${newTime} in seconds`;
+        return newTime;
+      });
+    }, 1000);
 
-  function increaseCounter() {
-    setDetails((prev) => ({
-      ...prev,
-      counter: prev.counter + 1,
-    }));
-  }
-
-  function handleName(e) {
-    setDetails((prev) => ({
-      ...prev,
-      name: e.target.value.,
-    }));
-  }
+    return () => {
+      console.log("clear");
+      clearInterval(timer);
+    };
+  }, [time]);
   return (
     <div className="App">
-      <input type="text" onChange={handleName} />
-      <h1>
-        {details.name} has Clicked {details.counter}
-      </h1>
-      <button onClick={increaseCounter}>Increase</button>
+      <h1> {time} in time-seconds </h1>
     </div>
   );
 }
