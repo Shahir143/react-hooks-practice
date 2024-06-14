e34fa87edf683b4c83085331d6b4b8c5dedc8b95
shahir143 <Shahirsyed143@gmail.com>
2024-06-14 13:07:38 +0530

    useRef function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
-import LoginProvider from "./createContext";
-import SinglePost from "./singlePost";
 
 function App() {
+  let setName = useRef(null);
+  let [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+    console.log(setName.current.value);
+  }
+
   return (
-    <LoginProvider>
-      <div className="App">
-        <SinglePost />
-      </div>
-    </LoginProvider>
+    <div className="App">
+      <input type="text" ref={setName} />
+      <h1>
+        {setName.current ? setName.current.value : "No One"} has Clicked {count}{" "}
+        times
+      </h1>
+      <button onClick={increaseCount}>Increase</button>
+    </div>
   );
 }
 
