7be07a59a5a85231d09ac9cadfc09ee5d3bc4985
shahir143 <Shahirsyed143@gmail.com>
2024-06-14 18:27:32 +0530

    useReducer function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useReducer } from "react";
 import "./App.css";
+import { type } from "@testing-library/user-event/dist/type";
+const initialState = { count: 0 };
+const ACTION = {
+  INCREASE: "increase",
+  DECREASE: "decrease",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREASE:
+      return { count: state.count + 1 };
 
+    case ACTION.DECREASE:
+      return { count: state.count > 0 ? state.count - 1 : 0 };
+
+    default:
+      return state;
+  }
+};
 function App() {
-  let setName = useRef(null);
-  let [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   function increaseCount() {
-    setCount(count + 1);
-    console.log(setName.current.value);
+    dispatch({ type: ACTION.INCREASE });
+  }
+  function DecreaseCount() {
+    dispatch({ type: ACTION.DECREASE });
   }
 
   return (
     <div className="App">
-      <input type="text" ref={setName} />
-      <h1>
-        {setName.current ? setName.current.value : "No One"} has Clicked {count}{" "}
-        times
-      </h1>
+      <h1>{state.count}</h1>
       <button onClick={increaseCount}>Increase</button>
+      <button onClick={DecreaseCount}>Increase</button>
     </div>
   );
 }
