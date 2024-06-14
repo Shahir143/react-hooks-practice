a3bc8f3108e8d05dea7cedc1428c7022872f8406
shahir143 <Shahirsyed143@gmail.com>
2024-06-14 18:43:42 +0530

    useLayoutEffect function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import React, { useRef, useReducer } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import "./App.css";
-import { type } from "@testing-library/user-event/dist/type";
-const initialState = { count: 0 };
-const ACTION = {
-  INCREASE: "increase",
-  DECREASE: "decrease",
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTION.INCREASE:
-      return { count: state.count + 1 };
 
-    case ACTION.DECREASE:
-      return { count: state.count > 0 ? state.count - 1 : 0 };
-
-    default:
-      return state;
-  }
-};
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [toggle, setToggle] = useState(false);
 
-  function increaseCount() {
-    dispatch({ type: ACTION.INCREASE });
-  }
-  function DecreaseCount() {
-    dispatch({ type: ACTION.DECREASE });
-  }
+  useEffect(() => {
+    console.log("useEffect");
+  }, [toggle]);
 
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect");
+  }, [toggle]);
+
+  function HandleClick(event) {
+    event.preventDefault();
+    setToggle((prevToggle) => !prevToggle);
+  }
   return (
     <div className="App">
-      <h1>{state.count}</h1>
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={DecreaseCount}>Increase</button>
+      {toggle && <h1>hello</h1>}
+      <button onClick={HandleClick}>Increase</button>
     </div>
   );
 }
